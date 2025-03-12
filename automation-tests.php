0fc8a69456a14bb650e62c1a2b05812eab95768f
Jason Bahl <jasonbahl@mac.com>
2025-03-12 13:44:01 -0600

    fix: fire emoji

--- automation-tests.php
+++ automation-tests.php
@@ -14,7 +14,7 @@ require_once __DIR__ . '/constants.php';
 
 // show a message in the admin dashboard with the plugin name and version
 add_action( 'admin_notices', function() {
-	echo '<div class="notice notice-info"><p>⚡ Automation Tests v' . AUTOMATION_TESTS_VERSION . '</p></div>';
+	echo '<div class="notice notice-info"><p>🔥 Automation Tests v' . AUTOMATION_TESTS_VERSION . '</p></div>';
 });
 
 
